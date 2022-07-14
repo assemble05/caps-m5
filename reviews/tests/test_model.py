3c5155da4d953ff2c5588a5ee24851b67615d118
Guilherme <guilherme.llopreti@gmail.com>
2022-07-14 15:31:19 -0300

    feat: review test class

---
+++ reviews/tests/test_model.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from reviews.models import Review
+from users.models import User
+
+
+class ReviewModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        ...
