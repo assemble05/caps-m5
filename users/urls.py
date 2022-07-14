9dc1b1c0cdd40268678ac6a2aaf0ef1691aaa7af
Fábio <fabioalmeida0896@gmail.com>
2022-07-14 17:46:30 -0300

    feat: created route account/id

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from users.views import ListCreateAccount, LoginView
+from users.views import ListCreateAccount, LoginView, ListAccountDetail
+
 
 
 urlpatterns = [
     path('accounts/', ListCreateAccount.as_view(), name="list_create_user"),
     path('login/', LoginView.as_view()),
+    path("accounts/<pk>/", ListAccountDetail.as_view(), name="list_account_view")
 ]
