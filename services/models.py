e4ef54a2c54ca9466fa372fabf3a95cd4e9ecf0e
Guilherme <guilherme.llopreti@gmail.com>
2022-07-15 14:44:44 -0300

    fix: service and provider relation

--- services/models.py
+++ services/models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.db import models
+from pyexpat import model
 
 
 # Create your models here.
@@ -11,7 +12,13 @@ class Service(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     is_active = models.BooleanField(default=True)
     is_delete = models.BooleanField(default=False)
-    id_provider = models.CharField(max_length=255, null=True)
+
+    provider = models.ForeignKey(
+        "users.User",
+        on_delete=models.SET_NULL,
+        related_name="worked_services",
+        null=True,
+    )
 
     address = models.ForeignKey(
         "adresses.Address", on_delete=models.CASCADE, related_name="services", null=True
