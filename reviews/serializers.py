186d2d50c146935214a35b8a5215a80234bd20b8
Guilherme <guilherme.llopreti@gmail.com>
2022-07-17 15:04:06 -0300

    fix: update review view, serializer and tests, and reajust reviews list, delete and filter tests

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -34,35 +34,3 @@ class ReviewSerializer(serializers.ModelSerializer):
             "user_criticized",
             "service",
         ]
-
-
-class UpdateReviewSerializer(serializers.ModelSerializer):
-    user_critic = CriticSerializer(read_only=True)
-    user_criticized = CriticSerializer(read_only=True)
-    service = ServiceSerializer(read_only=True)
-    user_criticized_id = serializers.UUIDField(write_only=True)
-
-    class Meta:
-        model = Review
-        fields = [
-            "id",
-            "stars",
-            "description",
-            "user_critic",
-            "user_criticized_id",
-            "user_criticized",
-            "service",
-        ]
-
-    def update(self, instance, validated_data):
-
-        if "user_criticized_id" in validated_data.keys():
-            user_criticized_id = validated_data.pop("user_criticized_id")
-            instance.user_criticized = User.objects.get(id=user_criticized_id)
-            instance.save()
-
-        for key, val in validated_data.items():
-            setattr(instance, key, val)
-        instance.save()
-
-        return instance
--- reviews/tests/test_views.py
+++ reviews/tests/test_views.py
@@ -1,10 +1,9 @@
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 from reviews.models import Review
-from reviews.serializers import ReviewSerializer, UpdateReviewSerializer
+from reviews.serializers import ReviewSerializer
 from services.models import Service
 from users.models import User
-from users.serializers import UserRegisterSerializer
 
 
 class ReviewCreateViewTest(APITestCase):
@@ -131,151 +130,139 @@ class ReviewCreateViewTest(APITestCase):
         )
 
 
-# class ReviewViewsTests(APITestCase):
-#     @classmethod
-#     def setUpTestData(cls):
-#         cls.user_critic = User.objects.create_user(
-#             email="goleiro_cassio@mail.com",
-#             password="123546",
-#             first_name="Goleiro",
-#             last_name="Cassioo",
-#             is_provider=False,
-#             phone="1140028922",
-#         )
-#         cls.critic_token = Token.objects.create(user=cls.user_critic)
+class ReviewViewsTests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.contractor = User.objects.create_user(
+            email="goleiro_cassio@mail.com",
+            password="123546",
+            first_name="Goleiro",
+            last_name="Cassioo",
+            is_provider=False,
+            phone="1140028922",
+        )
+        cls.contractor_token = Token.objects.create(user=cls.contractor)
 
-#         cls.user_criticized = User.objects.create_user(
-#             email="roni_calma@mail.com",
-#             password="123546",
-#             first_name="Roni",
-#             last_name="Calma",
-#             is_provider=True,
-#             phone="1189224002",
-#         )
-#         cls.criticized_token = Token.objects.create(user=cls.user_criticized)
+        cls.provider = User.objects.create_user(
+            email="roni_calma@mail.com",
+            password="123546",
+            first_name="Roni",
+            last_name="Calma",
+            is_provider=True,
+            phone="1189224002",
+        )
+        cls.provider_token = Token.objects.create(user=cls.provider)
 
-#         cls.first_review = Review.objects.create(
-#             description="Poderia ter trabalhado melhor!",
-#             user_critic=cls.user_critic,
-#             user_criticized=cls.user_criticized,
-#         )
+        cls.service = Service.objects.create(
+            title="Concerto de pia",
+            description="Preciso de concerto na pia da cozinha da minha casa.",
+            price=80.00,
+            contractor=cls.contractor,
+            provider=cls.provider,
+        )
 
-#         Review.objects.create(
-#             stars=5,
-#             description="Muito bom, sem reclamações!",
-#             user_critic=cls.user_critic,
-#             user_criticized=cls.user_criticized,
-#         )
+        cls.first_review = Review.objects.create(
+            description="Poderia ter trabalhado melhor!",
+            user_critic=cls.contractor,
+            user_criticized=cls.provider,
+            service=cls.service,
+        )
 
-#     def test_anyone_can_list_one_user_critics_received(self):
-#         response = self.client.get(f"/api/accounts/{self.user_criticized.id}/reviews/")
+        cls.second_review = Review.objects.create(
+            stars=5,
+            description="Muito bom, sem reclamações!",
+            user_critic=cls.provider,
+            user_criticized=cls.contractor,
+            service=cls.service,
+        )
 
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(
-#             ReviewSerializer(Review.objects.all(), many=True).data, response.data
-#         )
+    def test_anyone_can_list_one_user_received_reviews(self):
+        response = self.client.get(
+            f"/api/accounts/{self.provider.id}/received_reviews/"
+        )
 
-#     def test_auth_user_can_filter(self):
-#         self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.criticized_token.key}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            [ReviewSerializer(instance=self.first_review).data], response.data
+        )
 
-#         response = self.client.get(f"/api/reviews/{self.first_review.id}/")
+    def test_anyone_can_list_one_user_created_reviews(self):
+        response = self.client.get(
+            f"/api/accounts/{self.contractor.id}/created_reviews/"
+        )
 
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(
-#             ReviewSerializer(instance=self.first_review).data, response.data
-#         )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            [ReviewSerializer(instance=self.first_review).data], response.data
+        )
 
-#     def test_not_authenticated_user_cannot_filter(self):
-#         response = self.client.get(f"/api/reviews/{self.first_review.id}/")
+    def test_auth_user_can_filter(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.provider_token.key}")
 
-#         self.assertEqual(response.status_code, 401)
-#         self.assertEqual(
-#             response.data, {"detail": "Authentication credentials were not provided."}
-#         )
+        response = self.client.get(f"/api/reviews/{self.first_review.id}/")
 
-#     def test_review_owner_can_delete(self):
-#         self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.critic_token.key}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            ReviewSerializer(instance=self.first_review).data, response.data
+        )
 
-#         response = self.client.delete(f"/api/reviews/{self.first_review.id}/")
+    def test_not_authenticated_user_cannot_filter(self):
+        response = self.client.get(f"/api/reviews/{self.first_review.id}/")
 
-#         self.assertEqual(response.status_code, 204)
-#         self.assertEqual(Review.objects.all().count(), 1)
-#         self.assertNotIn(self.first_review, Review.objects.all())
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(
+            response.data, {"detail": "Authentication credentials were not provided."}
+        )
 
-#     def test_not_owner_cannot_delete_review(self):
-#         self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.criticized_token.key}")
+    def test_review_owner_can_delete(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.contractor_token.key}")
 
-#         response = self.client.delete(f"/api/reviews/{self.first_review.id}/")
+        response = self.client.delete(f"/api/reviews/{self.first_review.id}/")
 
-#         self.assertEqual(response.status_code, 403)
-#         self.assertEqual(
-#             response.data,
-#             {"detail": "You do not have permission to perform this action."},
-#         )
-#         self.assertIn(self.first_review, Review.objects.all())
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Review.objects.all().count(), 1)
+        self.assertNotIn(self.first_review, Review.objects.all())
 
-#     def test_owner_can_update_review(self):
-#         self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.critic_token.key}")
+    def test_not_owner_cannot_delete_review(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.provider_token.key}")
 
-#         update_data = {"stars": 4, "description": "Mandou bem!"}
+        response = self.client.delete(f"/api/reviews/{self.first_review.id}/")
 
-#         response = self.client.patch(
-#             f"/api/reviews/{self.first_review.id}/", update_data, format="json"
-#         )
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(
+            response.data,
+            {"detail": "You do not have permission to perform this action."},
+        )
+        self.assertIn(self.first_review, Review.objects.all())
 
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(response.json()["stars"], 4)
-#         self.assertEqual(response.json()["description"], "Mandou bem!")
-#         self.assertEqual(
-#             UpdateReviewSerializer(
-#                 instance=Review.objects.get(id=self.first_review.id)
-#             ).data,
-#             response.data,
-#         )
+    def test_owner_can_update_review(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.contractor_token.key}")
 
-#     def test_not_owner_cannot_update_review(self):
-#         self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.criticized_token.key}")
+        update_data = {"stars": 4, "description": "Mandou bem!"}
 
-#         update_data = {"stars": 5, "description": "O melhor de todos!"}
+        response = self.client.patch(
+            f"/api/reviews/{self.first_review.id}/", update_data, format="json"
+        )
 
-#         response = self.client.patch(
-#             f"/api/reviews/{self.first_review.id}/", update_data, format="json"
-#         )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["stars"], 4)
+        self.assertEqual(response.json()["description"], "Mandou bem!")
+        self.assertEqual(
+            ReviewSerializer(instance=Review.objects.get(id=self.first_review.id)).data,
+            response.data,
+        )
 
-#         self.assertEqual(response.status_code, 403)
-#         self.assertEqual(
-#             response.data,
-#             {"detail": "You do not have permission to perform this action."},
-#         )
+    def test_not_owner_cannot_update_review(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.provider_token.key}")
 
-#     def test_owner_can_change_the_review_criticized_user(self):
-#         self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.critic_token.key}")
+        update_data = {"stars": 5, "description": "O melhor de todos!"}
 
-#         another_user = User.objects.create_user(
-#             email="mano_magica@mail.com",
-#             password="123546",
-#             first_name="Mano",
-#             last_name="Magica",
-#             is_provider=True,
-#             phone="1189224002",
-#         )
-#         id = UserRegisterSerializer(instance=another_user).data["id"]
+        response = self.client.patch(
+            f"/api/reviews/{self.first_review.id}/", update_data, format="json"
+        )
 
-#         update_data = {"user_criticized_id": id}
-
-#         response = self.client.patch(
-#             f"/api/reviews/{self.first_review.id}/", update_data, format="json"
-#         )
-
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(
-#             Review.objects.get(id=self.first_review.id).user_criticized, another_user
-#         )
-#         self.assertNotIn(self.first_review, self.user_criticized.critics.all())
-#         self.assertIn(self.first_review, another_user.critics.all())
-#         self.assertEqual(
-#             UpdateReviewSerializer(
-#                 instance=Review.objects.get(id=self.first_review.id)
-#             ).data,
-#             response.data,
-#         )
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(
+            response.data,
+            {"detail": "You do not have permission to perform this action."},
+        )
